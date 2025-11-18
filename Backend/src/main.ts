002d14372932b13b5f17e5d170764f2646f24833
serge <125203998+serge740@users.noreply.github.com>
2025-11-19 01:35:55 +0300

    new chanegs

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin:[ process.env.CORS_ORIGIN , 'http://localhost:5173'],
+    origin:[ process.env.CORS_ORIGIN , 'http://localhost:5173','https://aby-bookings.onrender.com'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
