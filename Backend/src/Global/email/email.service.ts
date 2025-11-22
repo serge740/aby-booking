f2476c9179a6f47d3de8d1ae3d107cbe9d09ce04
serge <125203998+serge740@users.noreply.github.com>
2025-11-22 22:01:00 +0300

    added new changes

--- Backend/src/Global/email/email.service.ts
+++ Backend/src/Global/email/email.service.ts
@@ -10,8 +10,8 @@ export class EmailService {
 
   private transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST!,
-    port: parseInt(process.env.SMTP_PORT || '465'),
-    secure: true,
+    port: parseInt(process.env.SMTP_PORT || '587'),
+    secure: false,
     auth: {
       user: process.env.SMTP_USERNAME!,
       pass: process.env.SMTP_PASSWORD!,
