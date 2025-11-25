2e700fb0685b9087a3ebe319b98a877b9ec1ecd6
mihigojordan <mihigofololive@gmail.com>
2025-11-25 13:46:26 +0200

    hello

--- Backend/src/Modules/Company-management/auth/company-auth.controller.ts
+++ Backend/src/Modules/Company-management/auth/company-auth.controller.ts
@@ -45,8 +45,8 @@ export class CompanyAuthController {
 
       res.cookie('AccessCompanyToken', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        secure: false,
+        sameSite: 'lax',
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         path: '/',
       });
